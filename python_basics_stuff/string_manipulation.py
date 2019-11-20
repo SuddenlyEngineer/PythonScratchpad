80ac5dcae5223f155ce4b735a9e099bfd5b72e8f
SuddenlyEngineer <trhildebrand96@gmail.com>
2019-11-20 11:42:39 -0600

    Finished chapter 4 of the book. This file is done.

--- python_basics_stuff/string_manipulation.py
+++ python_basics_stuff/string_manipulation.py
@@ -95,3 +95,118 @@ string3 = " Cheeseburger "
 print(string1.lstrip())
 print(string2.rstrip())
 print(string3.strip())
+
+# Exercise 4:
+string1 = "Becomes"
+string2 = "becomes"
+string3 = "BEAR"
+string4 = "   bEautiful"
+
+print(string1.startswith("be"))
+print(string2.startswith("be"))
+print(string3.startswith("be"))
+print(string4.startswith("be"))
+
+# Exercise 5:
+string1 = string1.lower()
+string3 = string3.lower()
+string4 = string4.lstrip().lower()
+
+print(string1.startswith("be"))
+print(string2.startswith("be"))
+print(string3.startswith("be"))
+print(string4.startswith("be"))
+
+# SECTION 4.4
+
+prompt = "Hey, what's up? "
+user_input = input(prompt)
+print("You said:", user_input)
+
+response = input("What did you say? ")
+response = response.upper()
+print("Oh, I get it!", response)
+
+# Exercises follow here
+
+# Exercise 1:
+third_grade_insult = input("I know you are but what am I? ")
+print(third_grade_insult)
+
+# Exercise 2:
+print(third_grade_insult.lower())
+
+# Exercise 3:
+print(len(third_grade_insult))
+
+# SECTION 4.5 - CHALLENGE
+im_a_dolphin = input("Tell me your password: ")
+print("The first letter you entered was:", im_a_dolphin[0].upper())
+
+# SECTION 4.6
+num = input("Check these dubs: ")
+dubs = float(num) * 2
+print(dubs)
+
+# Exercises follow here
+
+# Exercise 1:
+running_out_of_ideas_here = "23"
+test = int(running_out_of_ideas_here) * 17
+print(test)
+
+# Exercise 2:
+running_out_of_ideas_here = "23"
+test = float(running_out_of_ideas_here) * 17
+print(test)
+
+# Exercise 3: 
+hours_of_sleep = 8
+recommendation = "You should get this many hours of sleep:"
+print(recommendation, str(hours_of_sleep))
+
+# Exercise 4:
+number1 = input("Gimme a number: ")
+number2 = input("How about another: ")
+print("The product of ",str(number1), "and ",str(number2), "is ", str(float(number1)*float(number2)))
+
+# SECTION 4.7
+
+# Exercises follow here
+
+# Exercise 1:
+weight = float(0.2)
+animal = "newt"
+print(str(weight),"kg is the weight of the",animal,".")
+
+# Exercise 2:
+print("{} kg is the weight of the {}.".format(weight, animal))
+
+# Exercise 3
+print(f"{weight} kg is the wieght of the {animal}.")
+
+# SECTION 4.8
+
+# Exercises follow here
+
+# Exercise 1:
+print("AAA".find("a"))
+
+# Exercise 2:
+print("Somebody said something to Samantha.".replace("s","x"))
+
+# Exercise 3:
+parsing = input("Gimme some text, one last time? ")
+letter = "e"
+print(parsing.find(letter))
+
+# SECTION 4.9 - CHALLENGE
+translation = input("Okay, I lied. Gimme some more text. ")
+translation = translation.replace("a","4")
+translation = translation.replace("b","8")
+translation = translation.replace("e","3")
+translation = translation.replace("l","1")
+translation = translation.replace("o","0")
+translation = translation.replace("s","5")
+translation = translation.replace("t","7")
+print(translation)
