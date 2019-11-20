8a2525b13fdf1264898b097ae58ed6fc74c8ea52
SuddenlyEngineer <trhildebrand96@gmail.com>
2019-11-20 12:15:22 -0600

    Initial commit of this. Finished with section 5.1.

---
+++ python_basics_stuff/numbers_and_math.py
@@ -0,0 +1,21 @@
+"""This code corresponds to Chapter 4 of the Python Basics Book"""
+
+# SECTION 5.1
+
+# Exercises follow here
+
+# Exercise 1:
+
+num1 = 25000000
+num2 = 25_000_000
+print(num1)
+print(num2)
+
+# Exercise 2:
+num = 1.75e5
+print(num)
+
+# Exercise 3:
+print(2e1000)
+
+# SECTION 5.2
