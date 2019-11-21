0c12ee0b4bc49c7bca6b22094292fb022ea4d77d
SuddenlyEngineer <trhildebrand96@gmail.com>
2019-11-21 01:19:56 -0600

    Onto chapter 6 of the book!

---
+++ python_basics_stuff/functions_and_loops.py
@@ -0,0 +1,13 @@
+"""This code corresponds to Chapter 6 of the Python Basics Book"""
+
+def multiply(x, y):
+    """Return the product of two numbers x and y."""
+    product = x * y
+
+def greet(name)
+    print(f"Hello, {name}!")
+
+num = multiply(2, 4)
+print(num)
+
+greet("Dave")
--- python_basics_stuff/numbers_and_math.py
+++ python_basics_stuff/numbers_and_math.py
@@ -1,4 +1,4 @@
-"""This code corresponds to Chapter 4 of the Python Basics Book"""
+"""This code corresponds to Chapter 5 of the Python Basics Book"""
 
 # SECTION 5.1
 
@@ -18,5 +18,28 @@ print(num)
 # Exercise 3:
 print(2e1000)
 
-# SECTION 5.2
+# SECTION 5.3 (5.2 had no exercises)
+
+base = input("Enter a base: ")
+exponential = input("Enter an exponent: ")
+result53 = float(base) ** float(exponential)
+print(f"{base} to the power of {exponential} = {result53}")
+
+# SECTION 5.5 (5.4 had no exercise)
+
+# Exercise 1
+num1 = input("Enter a number: ")
+num1round = round(float(num1), 2)
+print(f"{num1} rounded to 2 decimal places is {num1round}")
+
+# Exercise 2
+num2 = input("Enter a number: ")
+num2abs = abs(float((num2)))
+print(f"The absolute value of {num2} is {num2abs}")
+
+# Exercise 3
+num3 = input ("Enter a number: ")
+num32 = input("Enter another number: ")
+difference = (float(num3)-float(num32)).is_integer()
+print(f"The difference between {num3} and {num32} is an integer? {difference}!")
 
