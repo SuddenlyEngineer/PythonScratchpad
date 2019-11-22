9e493f189cf717651168d3ecffd9d0e0c5988ebb
SuddenlyEngineer <trhildebrand96@gmail.com>
2019-11-22 00:27:56 -0600

    Started working on tuples (Chapter 9)!

---
+++ python_basics_stuff/tuples_lists_dictionaries.py
@@ -0,0 +1,16 @@
+"""This code corresponds to Chapter 8 of the Python Basics Book"""
+
+# Section 9.1
+
+# Exercise 1
+
+cardinal_numbers = ("first", "second", "third")
+
+# Exercise 2
+
+print(cardinal_numbers[1])
+
+# Exercise 3
+
+position1, position2, position3 = cardinal_numbers
+
