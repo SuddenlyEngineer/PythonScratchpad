d57500ab158e95e32506a0607530aa259e7be1ab
SuddenlyEngineer <trhildebrand96@gmail.com>
2019-11-22 13:14:36 -0600

    Small commit. Was actually busy today.

--- python_basics_stuff/tuples_lists_dictionaries.py
+++ python_basics_stuff/tuples_lists_dictionaries.py
@@ -14,3 +14,20 @@ print(cardinal_numbers[1])
 
 position1, position2, position3 = cardinal_numbers
 
+print(position1)
+print(position2)
+print(position3)
+
+# Exercise 4
+my_name = tuple("Thomas")
+print(my_name)
+
+# Exercise 5
+x_test = "x" in my_name
+print(x_test)
+
+# Exercise 6
+new_name = my_name[1:len(my_name)] 
+print(new_name) 
+
+# Section 9.2
