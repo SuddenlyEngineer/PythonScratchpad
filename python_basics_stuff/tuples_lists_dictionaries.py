0a58d08ab1ad11363069161dd0041c558c8c6fe4
SuddenlyEngineer <trhildebrand96@gmail.com>
2019-11-22 23:08:34 -0600

    Continued work on tuples (Chapter 9)!

--- python_basics_stuff/tuples_lists_dictionaries.py
+++ python_basics_stuff/tuples_lists_dictionaries.py
@@ -1,5 +1,21 @@
 """This code corresponds to Chapter 8 of the Python Basics Book"""
 
+# Section 9.4 - FUNCTIONS
+
+def enrollment_stats(university_list):
+    """Takes an input of university names, enrolled students, and annual tuition fees, returning two lists of enrollment 
+    values and tuition fees. Simple as."""
+    enrollments = university_list[:][2]
+    tuition_rates = university_list[:][3]
+    return [enrollments, tuition_rates]
+
+def mean(list):
+    return mean(list)
+
+def median(list):
+    list.sort
+    return median(list)
+
 # Section 9.1
 
 # Exercise 1
@@ -31,3 +47,58 @@ new_name = my_name[1:len(my_name)]
 print(new_name) 
 
 # Section 9.2
+
+# Exercise 1
+food = ["rice", "beans"]
+
+# Exercise 2
+food.append("broccoli")
+
+# Exercise 3
+food.extend(["bread", "pizza"])
+
+# Exercise 4
+print(food[0:1])
+
+# Exercise 5
+print(food[len(food)-1])
+
+# Exercise 6
+breakfast = "eggs, fruit, orange juice".split(", ")
+
+# Exercise 7
+print(len(breakfast))
+
+# Section 9.3
+
+# Exercise 1
+
+data = ((1, 2), (3, 4))
+
+# Exercise 2
+
+for n in range(0,len(data)):
+    print(f"Row {n} sum: {sum(data[n])}")
+
+# Exercise 3
+numbers = [4, 3, 2, 1]
+
+# Exercise 4
+numbers2 = numbers[:]
+
+# Exercise 5
+numbers.sort()
+
+# Section 9.4 - CHALLENGE
+
+universities = [
+['California Institute of Technology', 2175, 37704],
+['Harvard', 19627, 39849],
+['Massachusetts Institute of Technology', 10566, 40732],
+['Princeton', 7802, 37000],
+['Rice', 5879, 35551],
+['Stanford', 19535, 40569],
+['Yale', 11701, 40500]
+]
+
+print(universities[2][:])
