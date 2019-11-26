f250a6c31ea9323fd65e8e59a6a3b6dda0e8c2bd
SuddenlyEngineer <trhildebrand96@gmail.com>
2019-11-26 08:09:28 -0600

    Did a bit of File I/O (Chapter 10).

--- python_basics_stuff/file_io.py
+++ python_basics_stuff/file_io.py
@@ -1,7 +1,28 @@
 """This code corresponds to Chapter 11 of the Python Basics Book"""
 
-output_file = open("test.txt", "w")
-output_file.writelines("The is my first file.")
-output_file.close()
+# Section 11.1
 
+# Exercise 1
+
+input_file = open("poem.txt", "r")
+for line in input_file.readlines():
+    print(line, end="")
+input_file.close()
+
+# Exercise 2
+with open("poem.txt", "r") as input_file:
+    for line in input_file.readlines():
+        print(line, end="")
+
+# Exercise 3
+
+with open("poem.txt", "r") as source_file, open("output.txt", "w") as dest:
+    for line in source_file.readlines():
+        dest.write(line)
+
+# Exercise 4
+
+input_file = open("output.txt", "a")
+input_file.writelines("\nSneed's Feed and Seed: Formerly Chuck's")
+input_file.close()
 
