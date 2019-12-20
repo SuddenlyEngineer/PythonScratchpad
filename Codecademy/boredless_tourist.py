721d2db12c65413c0a4e88088d31ed7facac57b6
SuddenlyEngineer <trhildebrand96@gmail.com>
2019-12-19 19:33:43 -0600

    Finished the boredless tourist.

--- Codecademy/boredless_tourist.py
+++ Codecademy/boredless_tourist.py
@@ -19,12 +19,29 @@ def add_attraction(destination, attraction):
   
   return attractions_for_destination
 
-def find_attraction(destination, interests):
+def find_attractions(destination, interests, attractions):
   destination_index = get_destination_index(destination)
-  
   attractions_in_city = attractions[destination_index]
   
   attractions_with_interest = []
+  
+  for attractions in attractions_in_city:
+    possible_attraction = attractions
+    attraction_tags = possible_attraction[1]
+    for interest in interests:
+      if interest in attraction_tags:
+        attractions_with_interest.append(possible_attraction[0])
+        
+  return attractions_with_interest
+
+def get_attractions_for_traveler(traveler, attractions):
+  traveler_destination = traveler[1]
+  traveler_interests = traveler[2]
+  traveler_attractions = find_attractions(traveler_destination, traveler_interests, attractions)
+  interests_string = "Hi " + traveler[0] + ", we think you'll like these places around " + traveler_destination + ": "
+  for attractions in traveler_attractions:
+    interests_string += attractions + " "
+  return interests_string
 
 destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
 
@@ -43,3 +60,11 @@ add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
 add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
+la_arts = find_attractions("Los Angeles, USA", ['art'], attractions)
+
+print(la_arts)
+
+smills_france = get_attractions_for_traveler(['Dereck Smill', 'Paris, France', ['monument']], attractions)
+
+print(smills_france)
